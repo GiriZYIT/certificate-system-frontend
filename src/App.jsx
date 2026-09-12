6023d0ee4a3c364b0e5a2f3eafc64a86dcd6d38a
GiriZYIT <itintern.zyveniq@gmail.com>
2026-09-12 18:58:07 +0530

    Update frontend verification page

--- src/App.jsx
+++ src/App.jsx
@@ -3,64 +3,34 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import AddIntern from "./pages/AddIntern";
 import InternList from "./pages/InternList";
-import Login from "./pages/login";
-
-function ProtectedRoute({ children }) {
-  const isLoggedIn = localStorage.getItem("adminLoggedIn");
-
-  if (!isLoggedIn) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return children;
-}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        {/* Public Login */}
-        <Route path="/login" element={<Login />} />
-
-        {/* Protected Dashboard */}
+        {/* Dashboard */}
         <Route
           path="/"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
+          element={<Dashboard />}
         />
 
-        {/* Protected Add */}
+        {/* Add Certificate */}
         <Route
           path="/add"
-          element={
-            <ProtectedRoute>
-              <AddIntern />
-            </ProtectedRoute>
-          }
+          element={<AddIntern />}
         />
 
-        {/* Protected Edit */}
+        {/* Edit Certificate */}
         <Route
           path="/edit/:certificate_id"
-          element={
-            <ProtectedRoute>
-              <AddIntern />
-            </ProtectedRoute>
-          }
+          element={<AddIntern />}
         />
 
-        {/* Protected Intern List */}
+        {/* Intern List */}
         <Route
           path="/interns"
-          element={
-            <ProtectedRoute>
-              <InternList />
-            </ProtectedRoute>
-          }
+          element={<InternList />}
         />
 
         {/* Unknown URL */}
